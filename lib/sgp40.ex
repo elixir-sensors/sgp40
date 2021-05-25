ff95abb5b2ca8ce13815c323b8ab7045047c0fce
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2021-05-25 18:52:50 -0400

    Init

---
+++ lib/sgp40.ex
@@ -0,0 +1,18 @@
+defmodule Sgp40 do
+  @moduledoc """
+  Documentation for `Sgp40`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Sgp40.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/sgp40_test.exs
@@ -0,0 +1,8 @@
+defmodule Sgp40Test do
+  use ExUnit.Case
+  doctest Sgp40
+
+  test "greets the world" do
+    assert Sgp40.hello() == :world
+  end
+end
