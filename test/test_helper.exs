3e7e7fafcbb44b36405acf431eeba2f87c8999b8
Masatoshi N <nishiguchi.masa@gmail.com>
2023-02-18 14:37:41 -0500

    Delete unnecessary version check in test helper

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,7 +1,5 @@
 # Always warning as errors
-if Version.match?(System.version(), "~> 1.10") do
-  Code.put_compiler_option(:warnings_as_errors, true)
-end
+Code.put_compiler_option(:warnings_as_errors, true)
 
 # Define dynamic mocks
 Mox.defmock(SGP40.MockTransport, for: SGP40.Transport)
